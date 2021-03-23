6fd0512a843c4522968f9c1b9846f3d28830718e
ParkVitna <bbn960903@gmail.com>
2021-03-23 21:39:05 +0900

    3월23일 오전오후수업

--- src/main/java/com/mycompany/webapp/controller/Exam04Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Exam04Controller.java
@@ -1,7 +1,7 @@
 package com.mycompany.webapp.controller;
 
+
 import java.sql.Connection;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -13,59 +13,111 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mycompany.webapp.dto.Board;
-import com.mycompany.webapp.service.Exam04Service;
+import com.mycompany.webapp.dto.Pager;
+import com.mycompany.webapp.service.BoardsService;
 
 @Controller
 @RequestMapping("/exam04")
 public class Exam04Controller {
-	@Autowired
-	private Exam04Service exam04Service;
-	
-	@Autowired
-	private DataSource dataSource;
-	
-	@RequestMapping("/content")
-	public String content(Model model) {
-		Connection conn = null;
-		try {
-			//커넥션 풀에서 커넥션 객체를 대여해 오기
-			conn = dataSource.getConnection();
-			model.addAttribute("connStatus", "성공");
-		}
-		catch(Exception e){
-			e.printStackTrace();
-			model.addAttribute("connStatus", "실패");
-		} finally {
-			try{
-				//커넥션 풀로 커넥션 객체를 반납하기
-				conn.close();
-			} catch(Exception e) {}
-		}
-		return "exam04/content";
-	}
-	
-	@GetMapping("/boards")
+   @Autowired
+   private BoardsService boardsService;
+   
+   @Autowired
+   private DataSource dataSource;
+   
+   
+   @RequestMapping("/content")
+   public String content(Model model){
+      Connection conn = null;
+      try {
+         //커넥션 풀에서 커넥션 객체를 대여해 오기
+         conn = dataSource.getConnection();
+         model.addAttribute("connStatus", "성공");
+      }catch(Exception e) {
+         e.printStackTrace();
+         model.addAttribute("connStatus", "실패");
+      }finally {
+         try {
+            //커넥션 풀로 커넥션 객체를 반납하기
+            conn.close();
+         } catch(Exception e) {}
+      }
+      return "exam04/content";
+   }
+   
+	/*@GetMapping("/list")
 	public String getBoardList(Model model) {
-		List<Board> list = new ArrayList<>();
-		for(int i=1; i<=10; i++) {
-			Board board = new Board();
-			board.setBno(i);
-			board.setBtitle("제목" + i);
-			board.setBcontent("내용" + i);
-			board.setBwriter("spring");
-			board.setBdate(new Date());
-			board.setBhitcount(0);
-			list.add(board);
-		}
-		model.addAttribute("list", list);
-		return "exam04/boardlist";
-	}
-	
-	@PostMapping("/boards")
-	public String saveBoard() {
-		return "redirect:/boards";
-	}
-
+	  List<Board> list = boardsService.getBoardList();
+	  model.addAttribute("list",list);
+	  return "exam04/boardlist";
+	}*/
+   
+   @GetMapping("/list")
+   public String getBoardList(@RequestParam(defaultValue="1") int pageNo, Model model) {
+      int totalRows = boardsService.getTotalRows();
+      Pager pager = new Pager(10,5, totalRows, pageNo);
+      List<Board> list = boardsService.getBoardList(pager);
+      model.addAttribute("list", list);
+      model.addAttribute("pager", pager);
+      return "exam04/boardlist";
+   }
+   
+   @GetMapping("/createForm")
+   public String insertForm() {
+      return "exam04/createForm";
+   }
+   
+	/* @PostMapping("/create")
+	 public String saveBoard(String btitle, String bcontent) {
+	   Board board = new Board();
+	   board.setBtitle(btitle);
+	   board.setBcontent(bcontent);
+	   board.setBwriter("user1");
+	   
+	   boardsService.saveBoard(board);
+	   
+	  return "redirect:/exam04/list";
+	 }*/
+   
+   @PostMapping("/create")
+	 public String saveBoard(Board board) {
+	   board.setBwriter("user1");
+	   boardsService.saveBoard(board);
+	  return "redirect:/exam04/list";
+	 }
+   
+   @GetMapping("/read")
+   public String read(int bno, Model model) {
+	   boardsService.addHitcount(bno);
+	   Board board = boardsService.getBoard(bno);
+	   model.addAttribute("board", board);
+	   return "exam04/read";
+   }
+   
+   @GetMapping("/updateForm")
+   public String updateForm(int bno, Model model) {
+	   Board board = boardsService.getBoard(bno);
+	   model.addAttribute("board", board);
+	   return "exam04/updateForm";
+   }
+   
+   @PostMapping("/update")
+	 public String update(Board board) {
+	   boardsService.updateBoard(board);
+	  return "redirect:/exam04/list";
+	 }
+   
+   @GetMapping("/delete")
+   public String delete(int bno) {
+	   boardsService.deleteBoard(bno);
+	   return "redirect:/exam04/list";
+   }
+   
+   
+   
+   
+   
 }
